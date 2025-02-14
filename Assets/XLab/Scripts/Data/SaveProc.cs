790f758451fae5af63c30a4a48b8b30aa6982133
Valentin Pavlov <v.pavlov@carx-tech.com>
2025-02-14 13:50:54 +0300

    rename

--- Assets/XLab/Scripts/Data/SaveProc.cs
+++ Assets/XLab/Scripts/Data/SaveProc.cs
@@ -5,20 +5,20 @@ using Xlab;
 
 public static class PlayerDataProc
 {
-	public static string pathFile => Path.Combine(Application.persistentDataPath, "playerSaveData.json");
+	public static string filePath => Path.Combine(Application.persistentDataPath, "playerSaveData.json");
 
 	public static void SavePlayer(PlayerData playerData)
 	{
 		var json = playerData.ToJson();
 		
-		File.WriteAllText(pathFile, json);
+		File.WriteAllText(filePath, json);
 	}
 
 	public static void LoadPlayer(PlayerData playerData)
 	{
-		if (File.Exists(pathFile))
+		if (File.Exists(filePath))
 		{
-			var json = File.ReadAllText(pathFile);
+			var json = File.ReadAllText(filePath);
 
 			playerData.FromJson(json);
 		}
